3dff2070e971b867c89af39f6b3d17e8ae12c270
黑色小猫咪 <dym3411795482@qq.com>
2026-04-16 12:25:09 +0800

    完成数据库创建功能

--- source/database/DB_settings.py
+++ source/database/DB_settings.py
@@ -1,5 +1,8 @@
-# 服务器数据库
+# 服务器数据库位置
 SERVER_DB_PATH = "./source/database/server_database/E2EE_sever.db"
 
-# 客户端数据库
-CLIENT_DB_PATH = "./source/database/client_database/E2EE_sever.db"
+# 客户端数据库位置
+CLIENT_DB_PATH = "./source/database/client_database/E2EE_client.db"
+
+# 客户端文件夹位置
+FILE_PATH = ""
--- source/database/database.py
+++ source/database/database.py
@@ -2,7 +2,64 @@ import sys
 import os
 import sqlite3
 
-from DB_settings import SERVER_DB_PATH
-conn = sqlite3.connect(SERVER_DB_PATH)
+from DB_settings import SERVER_DB_PATH,CLIENT_DB_PATH
 
-# def init_server_database():
+def init_server_database():
+    '''初始化服务器数据库'''
+
+    if not os.path.exists(SERVER_DB_PATH):
+
+        # 文件不存在 → 创建并初始化
+        conn = sqlite3.connect(SERVER_DB_PATH)
+
+        # 创建数据库
+        cursor = conn.cursor()
+        cursor.execute(
+                'CREATE TABLE users (               ' \
+                '   id INTEGER PRIMARY KEY,         ' \
+                '   username CHAR(20) UNIQUE,       ' \
+                '   ip CHAR(15),                    ' \
+                '   port CHAR(5),                   ' \
+                '   is_active BOOL DEFAULT false    ' \
+                ')                                  '
+            )
+        
+        conn.commit()
+        conn.close()
+
+    else:
+        pass
+
+def init_client_database():
+    '''初始化用户数据库'''
+
+    if not os.path.exists(CLIENT_DB_PATH):
+
+        # 文件不存在 → 创建并初始化
+        conn = sqlite3.connect(CLIENT_DB_PATH)
+
+        # 创建数据库
+        cursor = conn.cursor()
+        cursor.execute(
+                'CREATE TABLE friends (             ' \
+                '   id INTEGER PRIMARY KEY,         ' \
+                '   username CHAR(20) UNIQUE        ' \
+                ')                                  ' 
+            )
+        cursor.execute(
+                'CREATE TABLE chat_template (       ' \
+                '   id INTEGER PRIMARY KEY,         ' \
+                '   username CHAR(20) UNIQUE,       ' \
+                '   context VARCHAR(20) NOT NULL    ' \
+                ')                                  ' 
+            )
+        
+        conn.commit()
+        conn.close()
+        
+    else:
+        pass
+
+if __name__ == "__main__":
+    init_server_database()
+    init_client_database()
